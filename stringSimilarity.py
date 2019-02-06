d2d8636db9b817d034dfa44934a50cdb0403e7ea
Jack <jack.fw.holtby@gmail.com>
2019-02-06 23:56:54 +0800

    Got my similar() function working with values in the pandas df. Yay!

--- stringSimilarity.py
+++ stringSimilarity.py
@@ -8,3 +8,8 @@ testTable = pandas.read_csv('test.csv', index_col='test_id')
 
 
 for row in testTable.itertuples():
+    desc_x = row[1]
+    desc_y = row[2]
+    print(desc_x, desc_y)
+    print(similar(desc_x, desc_y))
+#    print(similar(row[2], row[3]))
