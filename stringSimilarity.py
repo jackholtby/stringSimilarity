e4a59e1ba9af7eba02516a2e1c25cbc2655418af
Jack <jack.fw.holtby@gmail.com>
2019-02-06 23:35:37 +0800

    Added similar() function to compare two strings

--- stringSimilarity.py
+++ stringSimilarity.py
@@ -1,6 +1,10 @@
 import pandas
 from difflib import SequenceMatcher
 
+def similar(first, second):
+    return SequenceMatcher(None, first, second).ratio()
+
 testTable = pandas.read_csv('test.csv', index_col='test_id')
+
 
 for row in testTable.itertuples():
