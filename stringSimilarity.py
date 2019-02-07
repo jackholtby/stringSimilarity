b8b7c01cc84c2305693f7d0c781143970cbed325
Jack <jack.fw.holtby@gmail.com>
2019-02-07 09:54:28 +0800

    Added calculated similarity scores and stored in same_security column

--- stringSimilarity.py
+++ stringSimilarity.py
@@ -1,15 +1,14 @@
 import pandas
 from difflib import SequenceMatcher
 
+# Function to generate a similarity metric between to strings
 def similar(first, second):
     return SequenceMatcher(None, first, second).ratio()
 
+# Create pandas DataFrame from the test.csv file.
+# Set test_id as the index.
 testTable = pandas.read_csv('test.csv', index_col='test_id')
 
-
-for row in testTable.itertuples():
-    desc_x = row[1]
-    desc_y = row[2]
-    print(desc_x, desc_y)
-    print(similar(desc_x, desc_y))
-#    print(similar(row[2], row[3]))
+# Add new column (I'm hoping you meant this column)
+# which has the metric of similarity stored in it.
+testTable['same_security'] = similar(testTable['description_x'], testTable['description_y'])
